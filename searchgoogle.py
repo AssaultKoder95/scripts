26928f2ba0cfeedba6b0ff85bbea003a4b0e755d
= <abhishek@coderin.me>
2016-07-08 22:33:57 +0530

    added doc string

--- searchgoogle.py
+++ searchgoogle.py
@@ -1,3 +1,14 @@
+"""
+Search google using firefox browser using cli
+Run this script with few default options to achieve better results
+Default options :
+1. use 1 as arguments to open google.co.in
+2. use 2 (space) string to search as arguments to google it
+
+More variations to add soon 
+
+"""
+
 import sys
 import webbrowser
 var = sys.argv[1]
